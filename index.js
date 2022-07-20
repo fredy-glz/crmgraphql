89c725c78f03ecd5bd6899e2e4928503aa6fbeb8
Alfredo Romero González <fredyglz.fr@gmail.com>
2022-07-20 02:48:51 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const { ApolloServer } = require('apollo-server');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: 'variables.env' });
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require('apollo-server-core');
+
+const typeDefs = require('./db/schema');
+const resolvers = require('./db/resolvers');
+const conectarDB = require('./config/db');
+
+// Conectar a la base de datos
+conectarDB();
+
+// Servidor
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  cors: {
+    origin: ['http://localhost:3000'],
+  },
+  context: ({ req }) => {
+    // console.log(req.headers['authorization']);
+    // console.log(req.headers);
+    const token = req.headers['authorization'] || '';
+    if (token) {
+      try {
+        const usuario = jwt.verify(
+          token.replace('Bearer ', ''),
+          process.env.SECRETA
+        );
+        return {
+          usuario,
+        };
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
+  plugins: [
+    ApolloServerPluginLandingPageGraphQLPlayground({
+      // options
+    }),
+  ],
+});
+
+server.listen({port : process.env.PORT || 4000}).then(({ url }) => {
+  console.log(`Server listen on ${url}`);
+});
